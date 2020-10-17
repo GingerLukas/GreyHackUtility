5359d2363b539429749ff012c9ba0a416da3b9a6
Lukáš Šafránek <safranek.lukas@ssakhk.cz>
2020-10-17 02:28:56 +0200

    added system image support

---
+++ GreyHackCompiler/GHTextConvertor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreyHackCompiler
+{
+    class GHTextConvertor
+    {
+        public static GHTextConvertor Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GHTextConvertor();
+                }
+
+                return _instance;
+            }
+        }
+
+        private static  GHTextConvertor _instance;
+
+
+        public GHTextConvertor()
+        {
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public Queue<char> HexToUtf16(Queue<char> queue, char cancelChar = ';')
+        {
+            StringBuilder tmp_hex = new StringBuilder();
+            Queue<char> output = new Queue<char>();
+            while (queue.Count>0 && queue.Peek()!=cancelChar)
+            {
+                tmp_hex.Append(queue.Dequeue());
+                if (tmp_hex.Length==4)
+                {
+                    output.Enqueue( (char) int.Parse(tmp_hex.ToString(), System.Globalization.NumberStyles.HexNumber));
+                    tmp_hex.Clear();
+                }
+            }
+
+            return output;
+        }
+    }
+}
--- GreyHackCompiler/Program.cs
+++ GreyHackCompiler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,11 @@ namespace GreyHackCompiler
         [STAThread]
         static void Main()
         {
+            string path =
+                @"C:\Users\lukas\OneDrive - Střední škola a vyšší odborná škola aplikované kybernetiky s.r.o\Plocha\GreyHack\Anonymous\images\part0.txt";
+            GHFileSystem.LoadFromString(File.ReadAllText(path));
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
